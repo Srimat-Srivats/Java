a25eb9d66f984b7ba01528f1a87cffa9a8b276fe
SRIMAT SRIVATS <srimatsrivats7@gmail.com>
2024-08-24 00:23:36 +0530

    project1b.java
    library class

---
+++ project2.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Book added: " + book);
+    }
+
+    public void removeBook(String isbn) {
+        books.removeIf(book -> book.getIsbn().equals(isbn));
+        System.out.println("Book removed with ISBN: " + isbn);
+    }
+
+    public Book searchBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public void displayAllBooks() {
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+}
